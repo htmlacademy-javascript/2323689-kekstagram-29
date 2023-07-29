04e5a9d25bde72b3d2c98941ff6227eea2f397f6
Keks <keks@htmlacademy.ru>
2023-07-29 12:21:24 +0300

    :heavy_check_mark: Сборка #9

---
+++ 9/js/data.js
@@ -0,0 +1,40 @@
+const getData = () => {
+  const nameMock = [
+    'Ivan',
+    'Fedor',
+    'Jack',
+    'Mark',
+    'ken',
+    'Bob',
+    'Alen',
+    'Den'
+  ];
+
+  const descriptionMock = [
+    'hello world',
+    'world is beautifull',
+    'life is good',
+    'sky is blue',
+    'sky is black',
+    'life is worst',
+    'goodbye world',
+    'where i am'
+
+  ];
+  const messageMock = [
+    'i am programmer',
+    'i am manager',
+    'i am shoes seller',
+    'i am engeneer',
+    'i am ofice worcker',
+    'i am driver',
+    'i am drug seller',
+    'i am doctor'
+
+  ];
+  return {nameMock,
+    descriptionMock,
+    messageMock
+  };
+};
+export{getData};
